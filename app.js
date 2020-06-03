6eda2024aa3c8350a4c7fddf93a62ac0a0e90864
EmersonBraun <emersonfbraun@gmail.com>
2020-06-03 18:32:58 -0300

    feat: basic application

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 var app = require('./config/server');
 
-app.listen(5000, function(){
+var server = app.listen(5000, function(){
     console.log('Server up');
 });
+
+var io = require('socket.io').listen(server)
+app.set('io', io)
+
+io.on('connection', (socket) =>{
+    console.log('User connected')
+    socket.on('disconnect', () => {
+        console.log('User disconnected')
+    })
+    socket.on('sendMsg', (data) => {
+        socket.emit('chatMsg', {name: data.name, msg: data.msg})
+    })
+    socket.broadcast.on('sendMsg', (data) => {
+        socket.emit('chatMsg', {name: data.name, msg: data.msg})
+    })
+    
+})
--- app/controller/chatController.js
+++ app/controller/chatController.js
@@ -1,7 +1,7 @@
 const { check, validationResult } = require('express-validator');
 
 module.exports.index = function(application, req, res) {
-  res.render("chat");
+  res.render("chat", {nick: ''})
 }
 
 module.exports.start = function(application, req, res) {
@@ -9,8 +9,8 @@ module.exports.start = function(application, req, res) {
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors.array())
-    return res.render("index", {validations: errors.array()}); 
+    return res.render("index", {validations: errors.array()})
   }
-  res.render("chat");
+  application.get('io').emit('chatMsg',{name: dadosForm.name, msg: 'Acabou de entrar'})
+  res.render("chat", {nick: dadosForm.name})
 }
